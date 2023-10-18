2110df872f838431fd9ba347e25decb68588819b
M Charity <mastermilkx@gmail.com>
2023-10-18 18:27:00 -0400

    move prototype with jump tech

--- Assets/Scripts/Player/MoveProto_1017.cs
+++ Assets/Scripts/Player/MoveProto_1017.cs
@@ -10,10 +10,11 @@ public class MoveProto_1017 : MonoBehaviour
     private float timeLerping = 0.0f;
     private bool inAir = true;
 
-    public float airSpeed = 10.0f;
-    public float groundSpeed = 7.0f;
-    public float lerpSmooth = 2.0f;
-    public float bufferAmt = 1.0f;
+    public float airSpeed = 12.0f;
+    public float groundSpeed = 3.0f;
+    public float lerpSmooth = 0.01f;
+    public float bufferAmt = 1.5f;
+    public float jumpForce = 3.0f;
 
 
     public LayerMask groundLayer;
@@ -23,11 +24,17 @@ public class MoveProto_1017 : MonoBehaviour
 
     private Vector2 velocity = Vector2.zero;
 
+
+    private SpriteRenderer sprRend;
+    public Color groundColor;
+    public Color airColor;
+
     // Start is called before the first frame update
     void Start(){
         rb = transform.GetComponent<Rigidbody2D>();
         endPt = transform.position;
         groundLayer = 1 << LayerMask.NameToLayer("Block");
+        sprRend = transform.GetComponent<SpriteRenderer>();
     }
 
     void Update(){
@@ -36,17 +43,44 @@ public class MoveProto_1017 : MonoBehaviour
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             endPt = new Vector2(mousePos.x,mousePos.y);
         }
+
+        // toggle being in the air or on the ground
+        if(Input.GetMouseButtonDown(1)){
+            inAir = !inAir;
+
+            // turn gravity on
+            if(!inAir){
+                rb.gravityScale = 2.0f;
+                sprRend.color = groundColor;
+            }
+            // turn gravity off
+            else{
+                endPt = rb.position;
+                rb.gravityScale = 0.0f;
+                sprRend.color = airColor;
+                rb.velocity = Vector2.zero;
+            }
+        }
+
+
         // move towards the currently selected point on lmb
-        if(inAir)
+        if(inAir){
             AirMove(endPt);
+        }else{
+            GroundMove();
+        }
     }
 
     // moves the character towards a specific direction (left: -1, right: 1, none: 0) with a specific magnitude (0-1)
     void AirMove(Vector2 toPos){
+        // make sure the gravity is off
+        rb.gravityScale = 0.0f;
+        sprRend.color = airColor;
+
         Vector2 curPos = new Vector2(rb.position.x, rb.position.y);
 
         // don't move if already there
-        if(toPos == null || InRange2D(curPos, endPt)){
+        if(toPos == null || InRange2D(curPos, endPt, 0.3f)){
             timeLerping = 0.0f;
             return;
         }
@@ -80,6 +114,17 @@ public class MoveProto_1017 : MonoBehaviour
 
         rb.MovePosition(movement);
         timeLerping += Time.deltaTime;
+    }
+
+    void GroundMove(){
+        // horizontal movement
+        float hor = Input.GetAxisRaw("Horizontal") * groundSpeed * Time.deltaTime; 
+        transform.Translate(hor * Vector2.right);
+
+        // allow jumps
+        if(Input.GetKeyDown(KeyCode.W)){
+            rb.AddForce(Vector3.up*jumpForce*rb.mass*100);
+        }
     }
 
     // checks if one point is close enough to another (X and Y axis)
